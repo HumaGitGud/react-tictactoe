89f52f05057751df827de89828acd5bdd2c13627
HumaGitGud <khomidov.huma@student.greenriver.edu>
2026-04-16 15:13:57 -0700

    created a tictactoe react practice game

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TicTacToe from "./TicTacToe"; 
+
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
